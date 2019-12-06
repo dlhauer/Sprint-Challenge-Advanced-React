9dcca20b08a4e304aa0f6dcaba2b3b3121532168
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-06 08:27:03 -0800

    API call, set up state

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PlayerList from './Components/PlayerList';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <PlayerList />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+      searchText: {
+        name: '',
+        country: '',
+        num_searches: '',
+      }
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:5000/api/players')
+      .then( response => {
+        console.log(response.data);
+        this.setState({ 
+          players: [...this.state.players, response.data]
+         })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PlayerList />
+      </div>
+    );
+  }
 }
 
 export default App;
