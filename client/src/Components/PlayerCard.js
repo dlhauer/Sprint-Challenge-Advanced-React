4cbfaeca0d2778f1a3cfb2c4216775e206c75b6f
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-06 08:16:32 -0800

    Component set-up, imports and exports

---
+++ client/src/Components/PlayerCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PlayerCard(props) {
+  return (
+    <h2>PlayerCard</h2>
+  );
+}
+
+export default PlayerCard;
---
+++ client/src/Components/PlayerList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PlayerCard from './PlayerCard';
+
+
+function PlayerList(props) {
+  return (
+    <PlayerCard />
+  );
+}
+
+export default PlayerList;
